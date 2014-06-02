201a224b88dd3cfd135c4cb636153b9b15777182
emthomas <titbabthomas@hotmail.com>
2014-06-02 09:08:26 -0400

    added libsvm.arff and test function

--- cs276_pa4/src/cs276/pa4/PairwiseLearner.java
+++ cs276_pa4/src/cs276/pa4/PairwiseLearner.java
@@ -142,7 +142,7 @@ Instances datasetPair = null;
 		attributesPair.add(new Attribute("body_w"));
 		attributesPair.add(new Attribute("header_w"));
 		attributesPair.add(new Attribute("anchor_w"));
-		attributesPair.add(new Attribute("class",Arrays.asList("+1","-1")));
+		attributesPair.add(new Attribute("class",Arrays.asList("-1","+1")));
 		datasetPair = new Instances("train_dataset_pair", attributesPair, 0);
 		//System.out.println("start merge:");
 		
@@ -270,6 +270,8 @@ Instances datasetPair = null;
 
 	@Override
 	public Classifier training(Instances dataset) {
+		System.out.println("IN TRAINING");
+		System.out.println(dataset);
 		try {
 			this.model.buildClassifier(dataset);
 		} catch (Exception e) {
@@ -403,5 +405,37 @@ Instances datasetPair = null;
 		}
 		
 		return result;
+	}
+	
+	//TODO: Remove. Only for test
+	public static void testRegressionModel() throws Exception{
+	
+		Instances data = new Instances(new BufferedReader(new FileReader("libsvm.arff")));
+		data.setClassIndex(data.numAttributes() - 1);
+		//build model
+		LibSVM model = new LibSVM();
+		model.setKernelType(new SelectedTag(LibSVM.KERNELTYPE_LINEAR, LibSVM.TAGS_KERNELTYPE));
+		//NaiveBayes model = new NaiveBayes();
+		model.buildClassifier(data); //the last instance with missing class is not used
+		System.out.println(data);
+		System.out.println(model);
+		//System.out.println("Num of Param: " + model.numParameters());
+        System.out.println("Weights:");
+        for (double coefficient : model.coefficients()) {
+			System.out.println(coefficient);
+		}
+		//classify the last instance
+		Instance myHouse = data.lastInstance();
+		double price = model.classifyInstance(myHouse);
+		System.out.println("My house ("+myHouse+"): "+price);
+		
+	}
+	
+	//TODO: Remove. Only for test
+	public static void main(String[] args) throws Exception{
+		//for(int i=0; i<10; i++) {
+		//	System.out.println((int)(Math.random()*100)+","+(int)(Math.random()*100)+","+(int)(Math.random()*100)+","+(int)(Math.random()*100)+","+(int)(Math.random()*100)+","+((int)(Math.random()*10)<5?"-1":"+1"));
+		//}
+		testRegressionModel();
 	}
 }
